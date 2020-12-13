eb9f2b23556bd23810ad5f0859753458cbcf1d33
Arthur_Simon <asimon@ms3-inc.com>
2020-12-13 10:27:06 +0200

    Initial commit

---
+++ src/main/java/auction_website/auctionwebsite/AuctionWebsiteApplication.java
@@ -0,0 +1,13 @@
+package auction_website.auctionwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/auction_website/auctionwebsite/AuctionWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package auction_website.auctionwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
