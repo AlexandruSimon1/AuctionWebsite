945639f2844366b0638c81630cb9f32c4063dee4
Arthur_Simon <asimon@ms3-inc.com>
2020-12-19 21:22:54 +0200

    Added exception for Not Found, Created Service and Controller for User, Added in entities the Entity anotation that wasn't been added before, Changed Persistance Config for liquibase found a error and wasn't working this implementation

--- src/main/java/com/auctionwebsite/config/PersistanceConfig.java
+++ src/main/java/com/auctionwebsite/config/PersistanceConfig.java
@@ -1,7 +1,6 @@
 package com.auctionwebsite.config;
 
 import liquibase.integration.spring.SpringLiquibase;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,23 +9,12 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistanceConfig {
-    //define this property in your embedded properties file or use spring's default
-//    @Value("${liquibase.change-log}")
-//    private String defaultLiquibaseChangelog;
     @Bean
     public DataSource getDataSource() {
         return DataSourceBuilder.create().driverClassName("com.mysql.cj.jdbc.Driver")
                 .url("jdbc:mysql://localhost:3306/auction_db?serverTimezone=UTC")
                 .username("root").password("Sanea@21041990").build();
     }
-
-//    @Bean
-//    public SpringLiquibase getSpringLiquiBase() {
-//        SpringLiquibase liquibase = new SpringLiquibase();
-//        liquibase.setChangeLog(defaultLiquibaseChangelog);
-//        liquibase.setDataSource(getDataSource());
-//        return liquibase;
-//    }
 
     @Bean
     public SpringLiquibase getSpringLiquibase () {
