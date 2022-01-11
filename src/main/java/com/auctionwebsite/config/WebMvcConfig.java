8109705d44b89383211b42e24dcd9198029b90cc
Arthur_Simon <asimon@ms3-inc.com>
2022-01-11 16:09:13 +0200

    Testing

--- src/main/java/com/auctionwebsite/config/WebMvcConfig.java
+++ src/main/java/com/auctionwebsite/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.auctionwebsite.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
@@ -16,6 +17,7 @@ import java.util.Collections;
 import java.util.stream.Collectors;
 
 @PropertySource("application-${spring.profiles.active}.properties")
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${ui.url.origin}")
@@ -25,5 +27,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins(myAllowedApi)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE").maxAge(3600);
+    }
+    @Bean
+    public SimpleCorsFilter corsFilter() {
+        CorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin(myAllowedApi);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        config.addAllowedMethod(HttpMethod.GET);
+        config.addAllowedMethod(HttpMethod.OPTIONS);
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.POST);
+        ((UrlBasedCorsConfigurationSource) source).registerCorsConfiguration("/**", config);
+        return new SimpleCorsFilter(source);
     }
 }
