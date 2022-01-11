bab267c5cb1b8c64b31bdf18c9334710af61128b
Arthur_Simon <asimon@ms3-inc.com>
2022-01-11 14:57:17 +0200

    Testing CORS Issues

--- src/main/java/com/auctionwebsite/config/WebMvcConfig.java
+++ src/main/java/com/auctionwebsite/config/WebMvcConfig.java
@@ -1,9 +1,15 @@
 package com.auctionwebsite.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @PropertySource("application-${spring.profiles.active}.properties")
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -13,8 +19,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(myAllowedApi).allowedHeaders("*")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+        registry.addMapping("/**").allowedOrigins(myAllowedApi)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE").maxAge(8400);
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(myAllowedApi));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
