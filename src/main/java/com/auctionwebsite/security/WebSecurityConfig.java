e193cce6b36aa00de7bb03e561d656dbccfd2b45
Arthur_Simon <asimon@ms3-inc.com>
2022-01-13 15:12:22 +0200

    Testing

--- src/main/java/com/auctionwebsite/security/WebSecurityConfig.java
+++ src/main/java/com/auctionwebsite/security/WebSecurityConfig.java
@@ -59,20 +59,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .headers()
-                // the headers you want here. This solved all my CORS problems!
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", myAllowedApi))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Max-Age", "3600"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization"))
-                .and()
+        http.csrf().disable().cors().and()
+//                .headers()
+//                // the headers you want here. This solved all my CORS problems!
+//                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", myAllowedApi))
+//                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH"))
+//                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Max-Age", "3600"))
+//                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
+//                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization"))
+//                .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/v1/auth/**").permitAll()
                 .antMatchers(HttpMethod.GET,"/api/v1/auctions/**").permitAll()
                 .antMatchers(HttpMethod.GET,"/api/v1/categories/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyRequest().authenticated().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
