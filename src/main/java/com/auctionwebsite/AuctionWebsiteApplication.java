db177420645c361593d6c86ea0a6c89182c2b79f
Arthur_Simon <asimon@ms3-inc.com>
2022-01-24 14:56:16 +0200

    Changed to another certificates

--- src/main/java/com/auctionwebsite/AuctionWebsiteApplication.java
+++ src/main/java/com/auctionwebsite/AuctionWebsiteApplication.java
@@ -1,7 +1,10 @@
 package com.auctionwebsite;
 
+import de.dentrassi.crypto.pem.PemKeyStoreProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.security.Security;
 
 @SpringBootApplication
 public class AuctionWebsiteApplication {
